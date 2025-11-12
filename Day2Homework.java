655b932dc9787377013e73c2a01e22141998d113
Anjana T S <anjanats2003@gmail.com>
2025-11-12 12:06:13 +0530

    day2Homework

---
+++ Day2Homework.java
@@ -0,0 +1,18 @@
+package java_study;
+
+public class Day2_Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String item_name="Soap";
+		int quantity = 4;
+		double quan = quantity;
+		double unit_price =  18.75;
+		double total_cost =  (quan* unit_price);
+		System.out.println("Item Nmae: "+item_name);
+		System.out.println("Quantity : "+quantity);
+		System.out.println("Unit Price: "+unit_price);
+		System.out.println("Total Cost: "+total_cost);
+	}
+
+}
